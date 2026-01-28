b1c789c170914b940bd5bbb7a5dce6166b97e8bd
Bhupender Kashyap <38746420+bhupenderkashyap122@users.noreply.github.com>
2026-01-28 17:56:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,17 +16,20 @@ io.on("connection", (socket) => {
 
   matchUser(socket);
 
+  /* ---------------- CHAT MESSAGE ---------------- */
   socket.on("message", (msg) => {
     if (socket.partner) {
       socket.partner.emit("message", msg);
     }
   });
 
+  /* ---------------- NEXT USER ---------------- */
   socket.on("next", () => {
     disconnectPartner(socket);
     matchUser(socket);
   });
 
+  /* ---------------- DISCONNECT ---------------- */
   socket.on("disconnect", () => {
     onlineUsers--;
     io.emit("onlineCount", onlineUsers);
@@ -34,12 +37,29 @@ io.on("connection", (socket) => {
     if (waitingUser === socket) waitingUser = null;
     disconnectPartner(socket);
   });
+
+  /* ---------------- WEBRTC SIGNALS ---------------- */
+  socket.on("offer", (offer) => {
+    if (socket.partner) socket.partner.emit("offer", offer);
+  });
+
+  socket.on("answer", (answer) => {
+    if (socket.partner) socket.partner.emit("answer", answer);
+  });
+
+  socket.on("ice-candidate", (candidate) => {
+    if (socket.partner) socket.partner.emit("ice-candidate", candidate);
+  });
 });
 
+/* ---------------- USER MATCHING ---------------- */
 function matchUser(socket) {
   if (waitingUser) {
     socket.partner = waitingUser;
     waitingUser.partner = socket;
+
+    socket.partnerCreatedOffer = false;
+    waitingUser.partnerCreatedOffer = false;
 
     socket.emit("matched");
     waitingUser.emit("matched");
@@ -51,6 +71,7 @@ function matchUser(socket) {
   }
 }
 
+/* ---------------- DISCONNECT PARTNER ---------------- */
 function disconnectPartner(socket) {
   if (socket.partner) {
     socket.partner.partner = null;
